0971e884cc3fc845074bef854a65e0fced5190a9
Niels <info@heimseiten.de>
2024-09-10 14:40:37 +0200

    Update default.php

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -1,4 +1,5 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_content']['linkWrapper'] = array ( 'Link um ganzes Element', 'Achtung: Es dürfen keine Links innerhalb des Textes oder im Bild sein.' );
+$GLOBALS['TL_LANG']['tl_content']['linkWrapper_legend'] = 'Element verlinken';
+$GLOBALS['TL_LANG']['tl_content']['linkWrapper'] = array ( 'Link um ganzes Element', 'Achtung: Es dürfen keine Links innerhalb des Textes oder im Bild existieren.' );
 $GLOBALS['TL_LANG']['tl_content']['openInNewWindow'] = array ( 'Link in neuen Fenster öffnen', '' );
